d371db1fa37ffef793420cbdd48619e50069b4b5
Jaime Radulovich <jradlov@gmail.com>
2018-07-11 08:03:16 -0700

    Add project files.

---
+++ RadioBtns_0/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+using System;
+
+namespace RadioBtns_0
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var radioGrp = FindViewById<RadioGroup>(Resource.Id.radioGroup1);
+
+            radioGrp.CheckedChange += delegate {
+                var btnTxt = FindViewById<RadioButton>(radioGrp.CheckedRadioButtonId).Text;
+
+                // prints to screen & saves to a file
+                //Log.Debug("----DEBUG----", "radioBtn id: " + radioGrp.CheckedRadioButtonId);
+                Log.Debug("----DEBUG----", "radioBtn text: " + btnTxt); 
+                //Console.WriteLine("radio btn clicked");  // this works too but doesn't save to a file
+            };
+        }
+    }
+}
+
